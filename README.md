88e731ffd2d788f58ceb0793ab0964ca20e42bbe
parkhyunwoo <parkhj0423@sungkyul.ac.kr>
2021-06-16 23:42:42 +0900

    fix: readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Run with `docker-compose up`.
 
 Hosts app at `localhost:3000` and api at `localhost:5000`.
+
+server 폴더에 .env 파일 생성 후 키값을 넣어야함
--- server/image.py
+++ server/image.py
@@ -1,6 +1,5 @@
 import base64
 
-
 def get_image_path(filepath):
     f = open(filepath, 'rb')
     base64_str = base64.b64encode(f.read()).decode('utf8')
